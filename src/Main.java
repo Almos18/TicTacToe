fc681b8bc82762a717c010e9d0b1177e225cf41e
almos18 <andrasbalint13@protonmail.com>
2023-11-17 16:12:46 +0000

    Creation of setValue function and checkWin function

--- src/Main.java
+++ src/Main.java
@@ -55,31 +55,29 @@ public class Main {
 
     public static boolean checkWin() {
 
-            if ((field[0][0] == 'X') && (field[0][1] == 'X') && (field[0][2] == 'X')) {
-                return true;
-            }
+        boolean checkHorizontals;
+        boolean checkVerticals;
+        boolean checkDiagonals;
+        checkVerticals = ((field[0][0] == 'X') && (field[0][1] == 'X') && (field[0][2] == 'X') ||
+                ((field[0][0] == 'O') && (field[0][1] == 'O') && (field[0][2] == 'O') ||
+                (field[1][0] == 'X') && (field[1][1] == 'X') && (field[1][2] == 'X') ||
+                ((field[1][0] == 'O') && (field[1][1] == 'O') && (field[1][2] == 'O') ||
+                (field[2][0] == 'X') && (field[2][1] == 'X') && (field[2][2] == 'X') ||
+                (field[2][0] == 'O') && (field[2][1] == 'O') && (field[2][2] == 'O'))));
 
-            else if ((field[0][0] == 'O') && (field[0][1] == 'O') && (field[0][2] == 'O')) {
-                return true;
-            }
+        checkHorizontals = ((field[0][0] == 'X') && (field[1][0] == 'X') && (field[2][0] == 'X') ||
+                ((field[0][0] == 'O') && (field[1][0] == 'O') && (field[2][0] == 'O') ||
+                (field[0][1] == 'X') && (field[1][1] == 'X') && (field[2][1] == 'X') ||
+                ((field[0][1] == 'O') && (field[1][1] == 'O') && (field[2][1] == 'O') ||
+                (field[0][2] == 'X') && (field[1][2] == 'X') && (field[2][2] == 'X') ||
+                (field[0][2] == 'O') && (field[1][2] == 'O') && (field[2][2] == 'O'))));
 
-            else if ((field[1][0] == 'X') && (field[1][1] == 'X') && (field[1][2] == 'X')) {
-                return true;
-            }
+        checkDiagonals = ((field[0][0] == 'X') && (field[1][1] == 'X') && (field[2][2] == 'X') ||
+                ((field[0][0] == 'O') && (field[1][1] == 'O') && (field[2][2] == 'O') ||
+                (field[2][0] == 'X') && (field[1][1] == 'X') && (field[0][2] == 'X') ||
+                ((field[2][0] == 'O') && (field[1][1] == 'O') && (field[0][2] == 'O'))));
 
-            else if ((field[1][0] == 'O') && (field[1][1] == 'O') && (field[1][2] == 'O')) {
-                return true;
-            }
+       return (checkVerticals || checkHorizontals || checkDiagonals);
 
-            else if ((field[2][0] == 'X') && (field[2][1] == 'X') && (field[2][2] == 'X')) {
-                return true;
-            }
-
-            else if ((field[2][0] == 'O') && (field[2][1] == 'O') && (field[2][2] == 'O')) {
-                return true;
-            }
-
-
-        return false;
     }
 }
