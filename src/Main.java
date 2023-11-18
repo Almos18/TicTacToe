8468cd8a6f345fafde35a531a477951da658fde8
almos18 <andrasbalint13@protonmail.com>
2023-11-18 18:32:41 +0000

    Remade part of checkWin() to be shorter and the program displays the winner letter now

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,49 @@ public class Main {
     {
         String input;
         char player = 'O';
-        boolean winner = false;
+        boolean stopRound = false;
+        String display = "";
 
-        while(!winner) {
-            input = JOptionPane.showInputDialog(null,"Where would O like to move? (Enter two numbers within the range 0-2)");
-            setValue(input, player);
-            player = 'X';
-
-            input = JOptionPane.showInputDialog(null, "Where would X like to move? (Enter two numbers within the range 0-2)");
-            setValue(input, player);
-            player = 'O';
-
-            winner = checkWin();
-            System.out.println(winner);
-
-            StringBuilder displayField = new StringBuilder();
-
-            for(int i=0;i<3;i++)
-            {
-                for(int j=0;j<3;j++)
-                {
-                    displayField.append(field[i][j]).append(" ");
-                }
-                displayField.append("\n");
-            }
-            JOptionPane.showMessageDialog(null, displayField.toString());
-        }
-
-
-        for(int i=0;i<3;i++)
+        while (!stopRound)
         {
-            for(int j=0;j<3;j++)
+            player = 'O';
+            display = "";
+            input = JOptionPane.showInputDialog(null, "Where would O like to move? " +
+                    "(Enter two numbers within the range 0-2)");
+            setValue(input, player);
+            displayField(display);
+            display = "";
+            stopRound = checkWin();
+
+            if(!stopRound)
             {
-                System.out.print(field[i][j] + " ");
+                player = 'X';
+            input = JOptionPane.showInputDialog(null, "Where would X like to move? " +
+                    "(Enter two numbers within the range 0-2)");
+            setValue(input, player);
+
+            checkWin();
             }
-            System.out.print("\n");
+
+            stopRound = checkWin();
+
+            displayField(display);
+            }
+        JOptionPane.showMessageDialog(null, display + "The winner is " + player);
         }
-    }
+
+        public static void displayField(String display)
+        {
+            for (int i=0; i<3; i++)
+            {
+                for (int j=0; j<3; j++)
+                {
+                    display += (field[i][j] + " ");
+                }
+                display += ("\n");
+            }
+            JOptionPane.showMessageDialog(null, display);
+        }
 
     public static void setValue(String input, char player)
     {
@@ -51,19 +57,19 @@ public class Main {
 
         while((x > 2 || x < 0) || (y > 2 || y < 0))
         {
-            input = JOptionPane.showInputDialog(null,"Out of bounds, enter two numbers within the range 0-2");
+            input = JOptionPane.showInputDialog(null,"Out of bounds, " +
+                    "enter two numbers within the range 0-2");
             x=Character.getNumericValue(input.charAt(0));
             y=Character.getNumericValue(input.charAt(1));
         }
 
         while(field[x][y] == 'X' || field[x][y] == 'O')
         {
-            input = JOptionPane.showInputDialog(null,"The space is already occupied, please enter another");
+            input = JOptionPane.showInputDialog(null,"The space is already occupied, " +
+                    "please enter another");
             x=Character.getNumericValue(input.charAt(0));
             y=Character.getNumericValue(input.charAt(1));
         }
-
-
         field[x][y] = player;
     }
 
@@ -72,12 +78,12 @@ public class Main {
         boolean checkHorizontals;
         boolean checkVerticals;
         boolean checkDiagonals;
-        checkVerticals = ((field[0][0] == 'X') && (field[0][1] == 'X') && (field[0][2] == 'X') ||
-                ((field[0][0] == 'O') && (field[0][1] == 'O') && (field[0][2] == 'O') ||
-                (field[1][0] == 'X') && (field[1][1] == 'X') && (field[1][2] == 'X') ||
-                ((field[1][0] == 'O') && (field[1][1] == 'O') && (field[1][2] == 'O') ||
-                (field[2][0] == 'X') && (field[2][1] == 'X') && (field[2][2] == 'X') ||
-                (field[2][0] == 'O') && (field[2][1] == 'O') && (field[2][2] == 'O'))));
+        checkVerticals = (((Character.isLetter(field[0][0])) && (Character.isLetter(field[0][1])) &&
+                (Character.isLetter(field[0][2]))) ||
+                ((Character.isLetter(field[1][0])) && (Character.isLetter(field[1][1])) &&
+                (Character.isLetter(field[1][2]))) ||
+                ((Character.isLetter(field[2][0])) && (Character.isLetter(field[2][1])) &&
+                (Character.isLetter(field[2][2]))));
 
         checkHorizontals = ((field[0][0] == 'X') && (field[1][0] == 'X') && (field[2][0] == 'X') ||
                 ((field[0][0] == 'O') && (field[1][0] == 'O') && (field[2][0] == 'O') ||
@@ -91,7 +97,8 @@ public class Main {
                 (field[2][0] == 'X') && (field[1][1] == 'X') && (field[0][2] == 'X') ||
                 ((field[2][0] == 'O') && (field[1][1] == 'O') && (field[0][2] == 'O'))));
 
-       return (checkVerticals || checkHorizontals || checkDiagonals);
+       return checkVerticals || checkHorizontals || checkDiagonals;
+
 
     }
 }
