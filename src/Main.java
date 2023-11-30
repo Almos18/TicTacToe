e3c886f00b8d5ac567496aa815a678fe0ec6d38f
almos18 <andrasbalint13@protonmail.com>
2023-11-30 18:44:11 +0000

    Fixed fake draw

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,14 @@ public class Main
     static char player = 'O';
     static int turn = 0;
     static boolean keepGoing = true;
+    String fontStyle = "Courier";
     JFrame jFrameWindow;
     JButton btn00, btn01, btn02, btn10, btn11, btn12, btn20, btn21, btn22;
 
     public Main()
     {
         //JButton btn00;
-        Variables var = new Variables();
+        //Variables var = new Variables();
         //var.setBtn00(btn00);
 
         jFrameWindow = new JFrame("TicTacToe");
@@ -69,13 +70,11 @@ public class Main
         btn22.addActionListener(selectMove);
 
         jFrameWindow.setVisible(true);
-
     }
 
     public static void main(String[] args)
     {
         Main guiApp = new Main();
-
     }
 
     private class DisplayField implements ActionListener
@@ -87,7 +86,7 @@ public class Main
 
             if(e.getSource()==btn00)
             {
-                btn00.setFont(new Font("Courier", Font.BOLD,90));
+                btn00.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn00.setText(playerAsString);
                 field[0][0] = player;
                 btn00.setEnabled(false);
@@ -97,7 +96,7 @@ public class Main
 
             else if(e.getSource()==btn01)
             {
-                btn01.setFont(new Font("Courier", Font.BOLD,90));
+                btn01.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn01.setText(playerAsString);
                 field[0][1] = player;
                 btn01.setEnabled(false);
@@ -107,7 +106,7 @@ public class Main
 
             else if(e.getSource()==btn02)
             {
-                btn02.setFont(new Font("Courier", Font.BOLD,90));
+                btn02.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn02.setText(playerAsString);
                 field[0][2] = player;
                 btn02.setEnabled(false);
@@ -117,7 +116,7 @@ public class Main
 
             else if(e.getSource()==btn10)
             {
-                btn10.setFont(new Font("Courier", Font.BOLD,90));
+                btn10.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn10.setText(playerAsString);
                 field[1][0] = player;
                 btn10.setEnabled(false);
@@ -127,7 +126,7 @@ public class Main
 
             else if(e.getSource()==btn11)
             {
-                btn11.setFont(new Font("Courier", Font.BOLD,90));
+                btn11.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn11.setText(playerAsString);
                 field[1][1] = player;
                 btn11.setEnabled(false);
@@ -137,7 +136,7 @@ public class Main
 
             else if(e.getSource()==btn12)
             {
-                btn12.setFont(new Font("Courier", Font.BOLD,90));
+                btn12.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn12.setText(playerAsString);
                 field[1][2] = player;
                 btn12.setEnabled(false);
@@ -147,7 +146,7 @@ public class Main
 
             else if(e.getSource()==btn20)
             {
-                btn20.setFont(new Font("Courier", Font.BOLD,90));
+                btn20.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn20.setText(playerAsString);
                 field[2][0] = player;
                 btn20.setEnabled(false);
@@ -157,7 +156,7 @@ public class Main
 
             else if(e.getSource()==btn21)
             {
-                btn21.setFont(new Font("Courier", Font.BOLD,90));
+                btn21.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn21.setText(playerAsString);
                 field[2][1] = player;
                 btn21.setEnabled(false);
@@ -167,7 +166,7 @@ public class Main
 
             else if(e.getSource()==btn22)
             {
-                btn22.setFont(new Font("Courier", Font.BOLD,90));
+                btn22.setFont(new Font(fontStyle, Font.BOLD,90));
                 btn22.setText(playerAsString);
                 field[2][2] = player;
                 btn22.setEnabled(false);
