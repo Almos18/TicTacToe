37d9ba5ba5a60ab3d8e7fb9654678d746cf67e03
almos18 <andrasbalint13@protonmail.com>
2024-01-23 18:53:29 +0000

    Fixed not resetting when X wins

--- src/Main.java
+++ src/Main.java
@@ -225,7 +225,6 @@ public class Main
         }
 
 
-
         checkDiagonals = (((field[0][0] == 'X') && (field[1][1] == 'X') && (field[2][2] == 'X')) ||
                 ((field[0][0] == 'O') && (field[1][1] == 'O') && (field[2][2] == 'O')) ||
                 ((field[2][0] == 'X') && (field[1][1] == 'X') && (field[0][2] == 'X')) ||
@@ -357,7 +356,6 @@ public class Main
             btn.setBackground(Color.BLUE);
 
         else
-
             btn.setBackground(Color.RED);
     }
 
